563a392c71dc847d220e5b806c09a2fec6d095e7
Robson Nakane <theblizzard1983@hotmail.com>
2026-05-16 14:23:46 -0300

    Revise package installations and add Flatpak setup
    Updated package installations and added Flatpak commands.

--- Atualização Opensuse Tumbleweed IceWN by distrobox.sh
+++ Atualização Opensuse Tumbleweed IceWN by distrobox.sh
@@ -14,41 +14,10 @@ sudo zypper -n patch-check; #Verificar por correções.
 sudo zypper -n verify; #Verificar a integridade das dependências dos pacotes.
 sudo grub2-mkconfig -o /boot/grub2/grub.cfg; ##Atualiza o grub
 
-#zypper -n in rofi blueman steam-devices distrobox podman fastfetch rsync gnome-boxes pcmanfm kitty dvd+rw-tools toolbox;
+zypper -n in rofi blueman fastfetch rsync gnome-boxes pcmanfm kitty dvd+rw-tools simple-scan thunderbird audacious gimp transmission-gtk rpi-imager firefox gwenview kate kdenlive yt-dlp audacity inkscape
 
-#flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo; ##Instalação dos pacotes Flatpak##
-
-##Após o reboot##
-        ##Criação dos containeres no distrobox##
-        ##Debian Testing##
-#distrobox create -Y --name debian --image debian:testing;
-        ##Archlinux##
-#distrobox create -Y --name archlinux --image archlinux:latest;
-        ##Fedora##
-#distrobox create -Y --name fedora --image fedora:latest;
-
-distrobox-upgrade --all;
-
-        ##Instalação dos pacotes nos containeres distrobox##
-#distrobox enter archlinux -- sudo pacman -S --noconfirm fastfetch simple-scan thunderbird audacious gimp transmission-gtk rpi-imager firefox gwenview kate kdenlive yt-dlp;
-
-#distrobox enter fedora -- sudo dnf5 install -y audacity inkscape;
-
-        ##Exportação dos pacotes instalados no distrobox archlinux##
-#distrobox enter archlinux -- distrobox-export --app simple-scan; distrobox enter archlinux -- distrobox-export --app thunderbird; distrobox enter archlinux -- distrobox-export --app audacious; distrobox enter archlinux -- distrobox-export --app gimp; distrobox enter archlinux -- distrobox-export --app transmission-gtk; distrobox enter archlinux -- distrobox-export --app rpi-imager; distrobox enter archlinux -- distrobox-export --app firefox; distrobox enter archlinux -- distrobox-export --app gwenview; distrobox enter archlinux -- distrobox-export --app kate; distrobox enter archlinux -- distrobox-export --app kdenlive;
-
-        ##Exportação dos pacotes instalados no distrobox fedora##
-#distrobox enter fedora -- distrobox-export --app audacity; distrobox enter fedora -- distrobox-export --app inkscape;
-
-        ##Instalação do lutris dentro do terminal distrobox debian##
-#echo -e "Types: deb\nURIs: https://download.opensuse.org/repositories/home:/strycore/Debian_12/\nSuites: ./\nComponents: \nSigned-By: /etc/apt/keyrings/lutris.gpg" | sudo tee /etc/apt/sources.list.d/lutris.sources > /dev/null
-#wget -q -O- https://download.opensuse.org/repositories/home:/strycore/Debian_12/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/lutris.gpg
-#sudo apt update;
-#sudo apt install -y lutris
-#distrobox-export --app lutris
-
-        ##Instalação dos pacotes flatpaks##
-#sudo flatpak install flathub com.spotify.Client -y; sudo flatpak install flathub com.valvesoftware.Steam -y; sudo flatpak install flathub us.zoom.Zoom -y; sudo flatpak install flathub org.onlyoffice.desktopeditors -y; sudo flatpak install flathub com.adobe.Flash-Player-Projector -y; sudo flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y; sudo flatpak install flathub org.chromium.Chromium -y; sudo flatpak install flathub org.fedoraproject.MediaWriter -y; sudo flatpak install flathub org.kde.kget -y; sudo flatpak install flathub org.videolan.VLC -y; sudo flatpak install flathub net.mkiol.SpeechNote -y; sudo flatpak install flathub com.saivert.pwvucontrol -y; sudo flatpak install flathub io.github.dvlv.boxbuddyrs -y;
+##Instalação dos pacotes Flatpak##
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo; sudo flatpak install flathub com.spotify.Client -y; sudo flatpak install flathub com.valvesoftware.Steam -y; sudo flatpak install flathub us.zoom.Zoom -y; sudo flatpak install flathub org.onlyoffice.desktopeditors -y; sudo flatpak install flathub com.adobe.Flash-Player-Projector -y; sudo flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y; sudo flatpak install flathub org.chromium.Chromium -y; sudo flatpak install flathub org.fedoraproject.MediaWriter -y; sudo flatpak install flathub org.kde.kget -y; sudo flatpak install flathub org.videolan.VLC -y; sudo flatpak install flathub net.mkiol.SpeechNote -y; sudo flatpak install flathub com.saivert.pwvucontrol -y; sudo flatpak install flathub io.github.dvlv.boxbuddyrs -y;
 
         ##Atualização do Flatpak##
 sudo flatpak update -y;
